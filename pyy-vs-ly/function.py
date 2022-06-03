890541a5c54031a075d179d7ae0c349bd607bedb
Thibault Mroz <thibault.mroz@grenoble-inp.org>
2022-06-03 10:52:55 +0200

    Changes + rapport added

--- pyy-vs-ly/function.py
+++ pyy-vs-ly/function.py
@@ -17,12 +17,11 @@ def split_atom_info(info):
 def extract_atoms_info(log_filename):
     """Extracts all infos from the log.lammps file. Returns an array"""
     info = []
-    run_thermo = int(split_atom_info_head(linecache.getline(log_filename,84))[2])
-    run = int(split_atom_info_head(linecache.getline(log_filename,100))[2])
+    run_thermo = int(split_atom_info_head(linecache.getline(log_filename,85))[2])
+    run = int(split_atom_info_head(linecache.getline(log_filename,101))[2])
     nb_values = int(run/run_thermo)
-    print(nb_values)
 
-    for i in range(115, 115+nb_values+1):
+    for i in range(116, 116+nb_values+1):
         temp = split_atom_info(linecache.getline(log_filename,i))
         temp = [i for i in temp if i != '']
         info.append(temp)
@@ -36,4 +35,3 @@ def get_pyy_ly(atoms_array):
         pyy.append(-float(atoms_array[i][2]))
         ly.append(float(atoms_array[i][4]))
     return pyy, ly
-
--- pyy-vs-ly/main.py
+++ pyy-vs-ly/main.py
@@ -2,16 +2,22 @@ from function import *
 import matplotlib.pyplot as plt 
 
 # Define Variables
-filename = r'log.lammps'
+filenames = [r'log.lammps-0,8', r'log.lammps-0,6', r'log.lammps-0,4', r'log.lammps-0,2', r'log.lammps-0,1', r'log.lammps']
 
 # Process
-info = extract_atoms_info(filename)
-pyy, ly = get_pyy_ly(info)
+for file in filenames:
+    info = extract_atoms_info(file)
+    pyy, ly = get_pyy_ly(info)
+    if file == filenames[5]:
+        plt.scatter(ly, pyy)
+        plt.plot(ly,pyy, label= '0,0 bin')
+    else :
+        plt.scatter(ly, pyy)
+        plt.plot(ly,pyy, label= '0,' + file[13] + ' bin')
 
 # Plot
-plt.scatter(ly, pyy)
-plt.plot(ly,pyy)
 plt.xlabel('ly')
 plt.ylabel('pyy')
+plt.legend(loc='best')
 plt.title('pyy vs ly')
 plt.show()
